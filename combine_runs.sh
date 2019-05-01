c57a053e751ba623f13f88f5fda0c9782748d1fd
cjfiscus <cfisc004@ucr.edu>
2019-04-30 22:24:42 -0700

    fixed combine runs

--- combine_runs.sh
+++ combine_runs.sh
@@ -9,19 +9,24 @@ source ./params
 # id samples with multiple seq runs
 LST=$(cut -f2 "$SEQ_LIST" | tail -n+2 | sort | uniq -d)
 
+cd "$OUT_DIR"
+
 # for each sample
 for i in $LST
 do
 	# get list of files to cat
 	FILES=$(awk -v i="$i" '$2 == i' "$SEQ_LIST" | cut -f1)
-	
+
+	echo "$FILES"
+
 	# cat files
-	cat $(sed 's/$/.txt/g' <<< "$FILES") | sort -k1 > temp.txt
+	zcat $(sed 's/$/.txt.gz/g' <<< "$FILES") | sort -k1 > temp.txt
 	
 	# aggregate counts
-	awk -F"\t" '{a[$1]+=$2;}END{for(i in a)print i"\t"a[i];}' temp.txt > "$OUT_DIR"/"$i".txt
+	awk -F"\t" '{a[$1]+=$2;}END{for(i in a)print i"\t"a[i];}' temp.txt | gzip > "$OUT_DIR"/"$i".txt.gz 
 
 	# cleanup
 	rm temp.txt
+	rm $(sed 's/$/.txt.gz/g' <<< "$FILES")
 done 
 
